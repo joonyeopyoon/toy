1ec124d22011398343aabea67a10f72a16cdb4c5
joonyeopyoon <yoon.jy@hanwha.com>
2023-05-27 17:18:59 +0900

    feat: 불필요 기능 제거

--- server.js
+++ server.js
@@ -3,13 +3,3 @@ console.log("Business logic");
 console.log("주문 기능");
 
 console.log("배송 기능");
-
-console.log("정산 기능");
-
-console.log("검색 기능");
-
-console.log("추천 기능");
-
-console.log("리뷰 기능");
-
-console.log("신고 기능");
